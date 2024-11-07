c5d9c332cefe02b90ceafddfdf67a2c53f8de145
Iakov Senatov <senatov@icloud.com>
2024-11-07 17:11:04 +0100

    synchro change both lists view with FS .(multithread w. semaphore)

--- MiMiNavigator/Models/FileSingleton.swift
+++ MiMiNavigator/Models/FileSingleton.swift
@@ -6,14 +6,25 @@
 //  Copyright © 2024 Senatov. All rights reserved.
 //
 import Combine
-
-import Combine
+import Foundation
 
 class FileSingleton: ObservableObject {
     static let shared = FileSingleton()
-
+    
     @Published public var leftFiles: [CustomFile] = [] // Files for the left panel
     @Published public var rightFiles: [CustomFile] = [] // Files for the right panel
-
+    
     private init() {}
+    
+    func updateLeftFiles(_ files: [CustomFile]) {
+        DispatchQueue.main.async {
+            self.leftFiles = files
+        }
+    }
+    
+    func updateRightFiles(_ files: [CustomFile]) {
+        DispatchQueue.main.async {
+            self.rightFiles = files
+        }
+    }
 }
