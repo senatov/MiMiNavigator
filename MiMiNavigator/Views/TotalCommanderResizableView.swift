e24a1faadec4d1e34f290a04eefa9f656254acf4
Senatov <senatov@icloud.com>
2025-01-05 22:57:13 +0100

    add Mnu "Files" and "Mark"

--- MiMiNavigator/Views/TotalCommanderResizableView.swift
+++ MiMiNavigator/Views/TotalCommanderResizableView.swift
@@ -97,8 +97,69 @@ struct TotalCommanderResizableView: View {
             .shadow(color: Color.black.opacity(0.2), radius: 4, x: 2, y: 2)
             .shadow(color: Color.white.opacity(0.7), radius: 4, x: -2, y: -2)
             .buttonStyle(.borderless)
+            Menu {
+                Button("Change Attributes", action: {})
+                Button("Pack...", action: {})
+                Button("Unpack Specific Files...", action: {})
+                Button("Test Archive(s)", action: {})
+                Button("Compare By Content", action: {})
+                Button("Associate With...", action: {})
+                Button("Internal Associations (MimiNav only)...", action: {})
+                Button("Properties...", action: {})
+                Button("Calculate Occupied Space...", action: {})
+                Button("Multi Rename Tool...", action: {})
+                Button("Edit Comme&nt...", action: {})
+                Button("Print", action: {})
+                Button("Split File...", action: {})
+                Button("Combine Files...", action: {})
+                Button("Encode File(MIME,UUE,XXE)...", action: {})
+                Button("Decode File(MIME,UUE,XXE,BinHex)...", action: {})
+                Button("Create Checksum...", action: {})
+                Button("Veruify Checksum...", action: {})
+                Button("Quit...", action: {})
+            } label: {
+                Label("Files", systemImage: "eye.circle")
+                    .padding(.horizontal, 10) // Отступы для лейбла
+                    .frame(maxWidth: .infinity, alignment: .leading) // Выравнивание по ширине лейбла
+                    .background(Color.blue.opacity(0.2)) // Фон для визуализации ширины
+                    .cornerRadius(8) // Скругленные углы
+            }
+            .fixedSize(horizontal: true, vertical: false) // Ограничение ширины меню до лейбла
+            .padding()
+
+            Menu {
+                Button("Select Group...", action: {})
+                Button("Unselect Group...", action: {})
+                Button("Select &All", action: {})
+                Button("Unselect All", action: {})
+                Button("Invert Selection...", action: {})
+                Button("Select All With Same Extension", action: {})
+                Button("Save Selection", action: {})
+                Button("Restore Selection", action: {})
+                Button("Save Selection to File", action: {})
+                Button("Load Selection from File", action: {})
+                Button("Copy Selected Names To Clipboard", action: {})
+                Button("Copy Names With Path To Clipboard...", action: {})
+                Button("Copy To Clipboard With All Details...", action: {})
+                Button("Copy To Clipboard With All Path+Details...", action: {})
+            } label: {
+                Label("Mark", systemImage: "pencil.circle")
+                    .padding(.horizontal, 10) // Отступы для лейбла
+                    .frame(maxWidth: .infinity, alignment: .leading) // Выравнивание по ширине лейбла
+                    .background(Color.blue.opacity(0.2)) // Фон для визуализации ширины
+                    .cornerRadius(8) // Скругленные углы
+            }
+            .fixedSize(horizontal: true, vertical: false) // Ограничение ширины меню до лейбла
+            .padding()
+
+            MenuButton(label: "Commands", systemImage: "doc.on.doc")
+            MenuButton(label: "Net", systemImage: "arrowshape.turn.up.forward")
+            MenuButton(label: "Show", systemImage: "trash.circle")
+            MenuButton(label: "Configuration", systemImage: "magnifyingglass.circle")
+            MenuButton(label: "Start", systemImage: "arrowshape.turn.up.forward")
+            MenuButton(label: "Help", systemImage: "arrowshape.turn.up.forward")
         }
-        .padding(.leading, 0.1)
+        .padding(.leading, 0.2)
         .padding(.bottom, 0.1)
     }
 
