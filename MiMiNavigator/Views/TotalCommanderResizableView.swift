ee8f206133ed3bc359c112ffd5a559d90ff64499
Iakov Senatov <senatov@icloud.com>
2025-02-18 15:52:32 +0100

    save stage

--- MiMiNavigator/Views/TotalCommanderResizableView.swift
+++ MiMiNavigator/Views/TotalCommanderResizableView.swift
@@ -191,8 +191,8 @@ struct TotalCommanderResizableView: View {
     private func buildDivider(geometry: GeometryProxy) -> some View {
         LogMan.log.debug("buildDivider()")
         return Rectangle()
-            .fill(Color.gray)
-            .frame(width: 7)
+            .fill(Color.blue)
+            .frame(width: 5)
             .gesture(
                 DragGesture()
                     .onChanged { value in
