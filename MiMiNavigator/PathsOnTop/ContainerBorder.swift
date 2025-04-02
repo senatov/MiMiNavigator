c78f91246595086befbbf9d09ef4a2353f3772b6
Senatov <senatov@icloud.com>
2025-04-02 13:41:39 +0200

    prepair to clean

--- MiMiNavigator/PathsOnTop/ContainerBorder.swift
+++ MiMiNavigator/PathsOnTop/ContainerBorder.swift
@@ -1,18 +1,25 @@
-//
-//  ContainerBorder.swift
-//  MiMiNavigator
-//
-//  Created by Iakov Senatov on 25.03.25.
-//  Copyright © 2025 Senatov. All rights reserved.
-//
+    //
+    //  ContainerBorder.swift
+    //  MiMiNavigator
+    //
+    //  Created by Iakov Senatov on 25.03.25.
+    //  Copyright © 2025 Senatov. All rights reserved.
+    //
 
 import SwiftUI
 
-// MARK: - Background and Border
+    // MARK: - Background and Border
 
 struct ContainerBorder: View {
     var body: some View {
         RoundedRectangle(cornerRadius: 6)
             .stroke(Color.gray.opacity(0.3), lineWidth: 1)
     }
+}
+
+#Preview {
+    ContainerBorder()
+        .frame(width: 200, height: 100)
+        .padding()
+        .background(Color.gray.opacity(0.1))
 }
