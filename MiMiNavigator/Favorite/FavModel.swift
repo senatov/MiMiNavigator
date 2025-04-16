6f3cd337458e0464f9d15e11014f9d4e548a6503
Iakov Senatov <senatov@icloud.com>
2025-04-16 15:37:59 +0200

    Fix. code III Stage

--- MiMiNavigator/Favorite/FavModel.swift
+++ MiMiNavigator/Favorite/FavModel.swift
@@ -9,43 +9,43 @@
 import Foundation
 
 class FavModel {
-    // Array to hold favorite directory URLs
-    var favoriteDirectories: [URL] = []
+  // Array to hold favorite directory URLs
+  var favoriteDirectories: [URL] = []
 
-    init() {
-        setupFavoriteDirectories()
+  init() {
+    setupFavoriteDirectories()
+  }
+
+  // MARK: -  Sets up default favorite directories, including iCloud, OneDrive, Google Drive, and network drives if available
+  private func setupFavoriteDirectories() {
+    let fileManager = FileManager.default
+    // Adding standard directories
+    favoriteDirectories.append(contentsOf: [
+      fileManager.documentsDirectory,
+      fileManager.cachesDirectory,
+      fileManager.temporaryDirectory,
+      fileManager.applicationSupportDirectory,
+      fileManager.libraryDirectory,
+      fileManager.downloadsDirectory,
+      fileManager.desktopDirectory,
+      fileManager.homeDirectory,
+      fileManager.musicDirectory,
+      fileManager.picturesDirectory,
+      fileManager.moviesDirectory,
+    ])
+
+    // Optionally add iCloud, OneDrive, Google Drive, and network drives if available
+    if let iCloud = fileManager.iCloudDirectory {
+      favoriteDirectories.append(iCloud)
     }
 
-    // MARK: -  Sets up default favorite directories, including iCloud, OneDrive, Google Drive, and network drives if available
-    private func setupFavoriteDirectories() {
-        let fileManager = FileManager.default
-        // Adding standard directories
-        favoriteDirectories.append(contentsOf: [
-            fileManager.documentsDirectory,
-            fileManager.cachesDirectory,
-            fileManager.temporaryDirectory,
-            fileManager.applicationSupportDirectory,
-            fileManager.libraryDirectory,
-            fileManager.downloadsDirectory,
-            fileManager.desktopDirectory,
-            fileManager.homeDirectory,
-            fileManager.musicDirectory,
-            fileManager.picturesDirectory,
-            fileManager.moviesDirectory,
-        ])
-
-        // Optionally add iCloud, OneDrive, Google Drive, and network drives if available
-        if let iCloud = fileManager.iCloudDirectory {
-            favoriteDirectories.append(iCloud)
-        }
-
-        if let oneDrive = fileManager.oneDriveDirectory {
-            favoriteDirectories.append(oneDrive)
-        }
-
-        if let googleDrive = fileManager.googleDriveDirectory {
-            favoriteDirectories.append(googleDrive)
-        }
-        favoriteDirectories.append(contentsOf: fileManager.networkDrives)
+    if let oneDrive = fileManager.oneDriveDirectory {
+      favoriteDirectories.append(oneDrive)
     }
+
+    if let googleDrive = fileManager.googleDriveDirectory {
+      favoriteDirectories.append(googleDrive)
+    }
+    favoriteDirectories.append(contentsOf: fileManager.networkDrives)
+  }
 }
