da81910f0952728cbbb326d5095715da200b1b00
Iakov Senatov <senatov@icloud.com>
2025-04-18 01:28:14 +0200

    code for fav tree new created

--- MiMiNavigator/Views/TotalCommanderResizableView.swift
+++ MiMiNavigator/Views/TotalCommanderResizableView.swift
@@ -7,44 +7,47 @@
 import SwiftUI
 
 struct TotalCommanderResizableView: View {
+
+    public let lineLimit = 230
     @State private var leftPanelWidth: CGFloat = 0
-    
+
     @State private var isShowMenu: Bool = UserPreferences.shared.restoreMenuState()
     @State private var selectedFile: CustomFile? = nil
     @State private var showTooltip: Bool = true
     @State private var tooltipPosition: CGPoint = .zero
     @State private var tooltipText: String = ""
-    
+
     @StateObject private var scanner = DualDirectoryScanner(
         leftDirectory: URL(fileURLWithPath: "/Users/senat/Downloads/Hahly"),
         rightDirectory: URL(fileURLWithPath: "/Users/senat")
     )
     @State private var leftPath: String = ""
     @State private var rightPath: String = ""
-    
+
     @State private var displayedLeftFiles: [CustomFile] = []
     @State private var displayedRightFiles: [CustomFile] = []
     @State private var favTreeStruct: [CustomFile] = []
-    
+
+
         // MARK: -
     @MainActor
     private func fetchPaths() async {
         leftPath = await scanner.leftDirectory.path
         rightPath = await scanner.rightDirectory.path
     }
-    
+
         // MARK: - Fetch the files asynchronously from the actor
     @MainActor
     private func fetchLeftFiles() async {
         displayedLeftFiles = await scanner.fileLst.getLeftFiles()
     }
-    
+
         // MARK: - Fetch the files asynchronously from the actor
     @MainActor
     private func fetchRightFiles() async {
         displayedRightFiles = await scanner.fileLst.getRightFiles()
     }
-    
+
         // MARK: -
     private func toggleMenu() {
         log.debug("toggleMenu()")
@@ -53,7 +56,7 @@ struct TotalCommanderResizableView: View {
             UserPreferences.shared.saveMenuState(isOpen: isShowMenu)
         }
     }
-    
+
         // MARK: -
     private func buildMainPanels(geometry: GeometryProxy) -> some View {
         log.debug("buildMainPanels()")
@@ -68,7 +71,7 @@ struct TotalCommanderResizableView: View {
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color.white)
     }
-    
+
     var body: some View {
         GeometryReader { geometry in
             ZStack {
@@ -90,7 +93,7 @@ struct TotalCommanderResizableView: View {
             }
         }
     }
-    
+
         // MARK: -
     private func buildLeftPanel(geometry: GeometryProxy) -> some View {
         log.debug("buildLeftPanel()")
@@ -152,28 +155,30 @@ struct TotalCommanderResizableView: View {
             }
         }
     }
-    
+
         // MARK: -
     private func builFavTreeMenu() -> some View {
         log.debug("builFavTreeMenu()")
         return TreeView(files: $favTreeStruct, selectedFile: $selectedFile)
-            .padding()
-            .frame(maxWidth: 180)
-            .font(.system(size: 14, weight: .regular))  // Унифицированный шрифт
+            .padding(3)
+            .frame(maxWidth: CGFloat(lineLimit))
+            .font(.custom("Helvetica Neue", size: 11).weight(.light))
+            .foregroundColor(Color(#colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1)))
+            .animation(.spring(response: 0.4, dampingFraction: 0.7, blendDuration: 0.3), value: favTreeStruct)
             .onAppear {
-                Task(priority: .low) {
+                Task(priority: .background) {
                     await fetchFavTree()
                 }
             }
     }
-    
+
     @MainActor
     private func fetchFavTree() async {
         log.debug("fetchFavTree()")
         let favScanner = FavScanner()
         favTreeStruct = favScanner.scanFavorites()
     }
-    
+
         // MARK: - Build Divider Between Panels
     private func buildDivider(geometry: GeometryProxy) -> some View {
         log.debug("buildDivider()")
@@ -198,7 +203,7 @@ struct TotalCommanderResizableView: View {
                 handleDoubleClickDivider(geometry: geometry)
             }
     }
-    
+
         // MARK: -
     private func handleDividerDrag(value: DragGesture.Value, geometry: GeometryProxy) {
         log.debug("handleDividerDrag")
@@ -217,20 +222,20 @@ struct TotalCommanderResizableView: View {
             showTooltip = true
         }
     }
-    
+
         // MARK: -
     private func handleDoubleClickDivider(geometry: GeometryProxy) {
         leftPanelWidth = geometry.size.width / 2
         UserDefaults.standard.set(leftPanelWidth, forKey: "leftPanelWidth")
     }
-    
+
         // MARK: -
     private func initializePanelWidth(geometry: GeometryProxy) {
         leftPanelWidth =
         UserDefaults.standard.object(forKey: "leftPanelWidth") as? CGFloat
         ?? geometry.size.width / 2
     }
-    
+
         // MARK: -
     private func buildDownToolbar() -> some View {
         log.debug("buildToolbar()")
@@ -242,7 +247,7 @@ struct TotalCommanderResizableView: View {
                     log.debug("View selected Docu")
                 }
             )
-            
+
             DownToolbarButtonView(
                 title: "F4 Edit",
                 systemImage: "pencil",
@@ -250,7 +255,7 @@ struct TotalCommanderResizableView: View {
                     log.debug("Edit button tapped")
                 }
             )
-            
+
             DownToolbarButtonView(
                 title: "F5 Copy",
                 systemImage: "document.on.document",
@@ -258,7 +263,7 @@ struct TotalCommanderResizableView: View {
                     log.debug("Copy button tapped")
                 }
             )
-            
+
             DownToolbarButtonView(
                 title: "F6 Move",
                 systemImage: "square.and.arrow.down.on.square",
@@ -266,7 +271,7 @@ struct TotalCommanderResizableView: View {
                     log.debug("Move button tapped")
                 }
             )
-            
+
             DownToolbarButtonView(
                 title: "F7 NewFolder",
                 systemImage: "folder.badge.plus",
@@ -274,7 +279,7 @@ struct TotalCommanderResizableView: View {
                     log.debug("NewFolder button tapped")
                 }
             )
-            
+
             DownToolbarButtonView(
                 title: "F8 Delete",
                 systemImage: "minus.rectangle",
@@ -282,7 +287,7 @@ struct TotalCommanderResizableView: View {
                     log.debug("Delete button tapped")
                 }
             )
-            
+
             DownToolbarButtonView(
                 title: "F4 Exit",
                 systemImage: "power",
@@ -290,7 +295,7 @@ struct TotalCommanderResizableView: View {
                     exitApp()
                 }
             )
-            
+
             DownToolbarButtonView(
                 title: "Settings",
                 systemImage: "gearshape",
@@ -298,7 +303,7 @@ struct TotalCommanderResizableView: View {
                     log.debug("Settings button tapped")
                 }
             )
-            
+
             DownToolbarButtonView(
                 title: "Console",
                 systemImage: "terminal",
@@ -310,13 +315,13 @@ struct TotalCommanderResizableView: View {
         .padding()
         .cornerRadius(3)
     }
-    
+
         // MARK: -
     private func exitApp() {
         log.debug("exitApp()")
         NSApplication.shared.terminate(nil)
     }
-    
+
         // MARK: -
     private func addKeyPressMonitor() {
         log.debug("addKeyPressMonitor()")
