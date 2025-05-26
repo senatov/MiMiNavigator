1e738249f32e003721b303407cd4c7bae53adf71
Iakov Senatov <senatov@icloud.com>
2025-05-26 02:41:16 +0200

    +folders

---
+++ MiMiNavigator/Models/Folders.swift
@@ -0,0 +1,20 @@
+//
+//  Folders.swift
+//  MiMiNavigator
+//
+//  Created by Iakov Senatov on 26.05.2025.
+//  Copyright © 2025 Senatov. All rights reserved.
+//
+import Combine
+import Foundation
+import SwiftUICore
+
+public class Folders: ObservableObject {
+
+    @Published var leftDirectory: URL = URL.documentsDirectory
+    @Published var rightDirectory: URL = URL.downloadsDirectory
+    @Published var selectedDirectory: URL = URL.documentsDirectory
+    @Published var leftFiles: [CustomFile] = []
+    @Published var rightFiles: [CustomFile] = []
+
+}
