4bc8408064ddd79638e836cf8adec15d8f23b9ed
Senatov <senatov@icloud.com>
2025-06-28 10:18:49 +0200

    + event-catch / logging

--- MiMiNavigator/Menus/BlurView.swift
+++ MiMiNavigator/Menus/BlurView.swift
@@ -11,12 +11,17 @@ import SwiftUI
 
 struct BlurView: NSViewRepresentable {
     @Binding var material: NSVisualEffectView.Material
+    var onClick: (() -> Void)? = nil
 
     func makeNSView(context: Context) -> NSVisualEffectView {
         let view = NSVisualEffectView()
         view.blendingMode = .withinWindow
         view.material = material
         view.state = .active
+
+        let clickGesture = NSClickGestureRecognizer(target: context.coordinator, action: #selector(Coordinator.handleClick))
+        view.addGestureRecognizer(clickGesture)
+
         return view
     }
 
@@ -24,6 +29,23 @@ struct BlurView: NSViewRepresentable {
         if nsView.material != material {
             nsView.material = material
             log.info("Material updated: \(material.rawValue)")
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(onClick: onClick)
+    }
+
+    class Coordinator: NSObject {
+        let onClick: (() -> Void)?
+
+        init(onClick: (() -> Void)?) {
+            self.onClick = onClick
+        }
+
+        @objc func handleClick() {
+            log.debug("BlurView clicked")
+            onClick?()
         }
     }
 }
