1ab60a05be3aa06a22f8e5b02acf58340a204c99
Iakov Senatov <senatov@icloud.com>
2025-08-07 11:42:32 +0200

    fix paths
    some Aaaa

--- Script/refreshVersionFile.zsh
+++ Script/refreshVersionFile.zsh
@@ -19,7 +19,7 @@ VERSION="$NOW at Host: $HOSTNAME"
 
 # ✅ Определение пути к целевому файлу
 SCRIPT_DIR="$(cd -- "$(dirname "$0")" && pwd)"
-TARGET_DIR="$HOME/Develop/MMNavigator/MiMiNavigator"
+TARGET_DIR="$HOME/Develop/MMNavigator/Gui"
 TARGET_FILE="${TARGET_DIR}/curr_version.asc"
 
 # ✅ Диагностика
