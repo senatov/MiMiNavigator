c43782a52555cba34adcece6c47518b06bf3020e
Iakov Senatov <senatov@icloud.com>
2025-08-12 03:01:36 +0200

    fix select file style

--- Gui/Sources/Views/FileRowView.swift
+++ Gui/Sources/Views/FileRowView.swift
@@ -1,10 +1,10 @@
-//
-//  Untitled.swift
-//  MiMiNavigator
-//
-//  Created by Iakov Senatov on 11.08.2025.
-//  Copyright © 2025 Senatov. All rights reserved.
-//
+    //
+    //  Untitled.swift
+    //  MiMiNavigator
+    //
+    //  Created by Iakov Senatov on 11.08.2025.
+    //  Copyright © 2025 Senatov. All rights reserved.
+    //
 
 import AppKit
 import SwiftUI
@@ -28,7 +28,12 @@ struct FileRowView: View {
         HStack {
             Image(nsImage: NSWorkspace.shared.icon(forFile: file.urlValue.path))
                 .resizable()
+                .interpolation(.high) // Improve visual quality for resized icons
                 .frame(width: FilePanelStyle.iconSize, height: FilePanelStyle.iconSize)
+                .shadow(color: .black.opacity(0.22), radius: 2, x: 0, y: 1) // Subtle drop shadow for depth
+                .contrast(1.12) // Slightly increase contrast
+                .saturation(1.06) // Slightly richer colors
+                .padding(.trailing, 6) // Breathing room between icon and text
             Text(file.nameStr)
                 .foregroundColor(nameColor)
         }
@@ -38,7 +43,7 @@ struct FileRowView: View {
             if isSelected {
                 Rectangle()
                     .fill(FilePanelStyle.selectedRowFill)
-                    .overlay(Rectangle().stroke(FilePanelStyle.selectedRowStroke, lineWidth: 1))
+                    .overlay(Rectangle().stroke(FilePanelStyle.selectedRowStroke, lineWidth: FilePanelStyle.selectedBorderWidth ))
             } else {
                 Color.clear
             }
