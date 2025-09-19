fab9596e13d9613d95667be7057ca2c8bf56390e
Senatov <senatov@icloud.com>
2025-09-19 13:48:33 +0200

    tmp

--- GUI/Sources/FilePanel/FilePanelView.swift
+++ GUI/Sources/FilePanel/FilePanelView.swift
@@ -16,21 +16,25 @@ struct FilePanelView: View {
     @Binding var leftPanelWidth: CGFloat
     /// Called when user clicks anywhere inside the panel (left/right)
     let onPanelTap: (PanelSide) -> Void
+
     // MARK: - Init
-    init(selectedSide: PanelSide,
-         geometry: GeometryProxy,
-         leftPanelWidth: Binding<CGFloat>,
-         fetchFiles: @escaping @Sendable @concurrent (PanelSide) async -> Void,
-         appState: AppState,
-         onPanelTap: @escaping (PanelSide) -> Void = { side in log.info("onPanelTap default for \(side)") })
-    {
+    init(
+        selectedSide: PanelSide,
+        geometry: GeometryProxy,
+        leftPanelWidth: Binding<CGFloat>,
+        fetchFiles: @escaping @Sendable @concurrent (PanelSide) async -> Void,
+        appState: AppState,
+        onPanelTap: @escaping (PanelSide) -> Void = { side in log.info("onPanelTap default for \(side)") }
+    ) {
+        log.info(#function + " for side \(selectedSide)" + " with leftPanelWidth: \(leftPanelWidth.wrappedValue.rounded())")
         self._leftPanelWidth = leftPanelWidth
         self.geometry = geometry
-        self._viewModel = StateObject(wrappedValue: FilePanelViewModel(
-            panelSide: selectedSide,
-            appState: appState,
-            fetchFiles: fetchFiles
-        ))
+        self._viewModel = StateObject(
+            wrappedValue: FilePanelViewModel(
+                panelSide: selectedSide,
+                appState: appState,
+                fetchFiles: fetchFiles
+            ))
         self.onPanelTap = onPanelTap
     }
 
@@ -38,7 +42,6 @@ struct FilePanelView: View {
     var body: some View {
         let currentPath = appState.pathURL(for: viewModel.panelSide)
         log.info(#function + " for side \(viewModel.panelSide) with path: \(currentPath?.path ?? "nil")")
-
         return VStack {
             PanelBreadcrumbSection(
                 panelSide: viewModel.panelSide,
@@ -57,9 +60,11 @@ struct FilePanelView: View {
                 }
             )
         }
-        .frame(width: viewModel.panelSide == .left
-            ? (leftPanelWidth > 0 ? leftPanelWidth : geometry.size.width / 2)
-            : nil)
+        .frame(
+            width: viewModel.panelSide == .left
+                ? (leftPanelWidth > 0 ? leftPanelWidth : geometry.size.width / 2)
+                : nil
+        )
         .panelFocus(panelSide: viewModel.panelSide) {
             log.info("Focus lost on \(viewModel.panelSide); clearing selection")
             viewModel.selectedFileID = nil
