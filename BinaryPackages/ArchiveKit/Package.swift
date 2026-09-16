02be49294f3f9f33918e10e38b0ef81ac1adb85a
Iakov Senatov <javaentwickler@gmail.com>
2026-09-16 02:51:16 +0200

    FIX: error "Missing package product", "no XCFramework и Cannot find ArchiveSessionStore"

--- BinaryPackages/ArchiveKit/Package.swift
+++ BinaryPackages/ArchiveKit/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     products: [.library(name: "ArchiveKit", targets: ["ArchiveKitDependencies"])],
     dependencies: [.package(path: "../LogKit"), .package(path: "../FileModelKit")],
     targets: [
-        .binaryTarget(name: "ArchiveKit", url: "https://github.com/senatov/MiMiNavigator/releases/download/kits-ffba754c7dd76553/ArchiveKit.xcframework.zip", checksum: "0d2d1e83d4bd4268dd18d3d41248db7495ca516ff87491916fec95b2aaefecaa"),
+        .binaryTarget(name: "ArchiveKit", path: "ArchiveKit.xcframework"),
         .target(name: "ArchiveKitDependencies", dependencies: ["ArchiveKit", .product(name: "LogKit", package: "LogKit"), .product(name: "FileModelKit", package: "FileModelKit")])
     ]
 )
